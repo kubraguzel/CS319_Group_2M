efa63c50fde4754bd9b3906855e1dc44867b0121
pelin17 <32205245+pelin17@users.noreply.github.com>
2017-12-14 18:48:28 +0300

    Add files via upload

---
+++ Codes/WorldMap.java
@@ -0,0 +1,116 @@
+
+
+package test;
+
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+/**
+* world map class that extends BasicGameState 
+* 
+* @param 
+* @return 
+*/
+
+public class WorldMap extends BasicGameState 
+{
+
+	/**
+	* construct of the world map class
+	* 
+	* @param worldmap
+	* @return 
+	*/
+
+	public WorldMap(int worldmap) 
+	{
+
+
+
+
+
+
+	}
+
+
+	/**
+	* init method
+	* 
+	* @param gc The GameContainer object
+	* @param sbg The StateBasedGame object
+	* @return 
+	*/
+
+	public void init(GameContainer gc, StateBasedGame sbg)
+			throws SlickException 
+			{
+
+
+
+
+
+	}
+
+	/**
+	* render method draws the buttons and screen
+	* 
+	* @param gc The GameContainer object
+	* @param sbg  The StateBasedGame object
+	* @param g The Graphics object
+	* @return 
+	*/
+
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
+			throws SlickException 
+			{
+
+
+
+
+	}
+
+	/**
+	* update method
+	* 
+	* @param gc The GameContainer object
+	* @param sbg The StateBasedGame object
+	* @param delta  
+	* @return 
+	*/
+
+	public void update(GameContainer gc, StateBasedGame sbg, int delta)
+			throws SlickException 
+			{	
+
+
+
+
+
+	}
+
+
+	/**
+	* getId method
+	* 
+	* @param
+	* @return 1
+	*/
+
+	public int getID() 
+	{
+
+		return 1;
+	}
+
+
+
+
+
+
+
+
+	
+}
