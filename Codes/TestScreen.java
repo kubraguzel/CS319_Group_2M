28c0fba3ab85d067972f3607db522617ad465b3a
semihteker <32205459+semihteker@users.noreply.github.com>
2017-12-16 03:35:51 +0300

    Add files via upload

--- Codes/TestScreen.java
+++ Codes/TestScreen.java
@@ -1,5 +1,9 @@
+package deneme;
 
+import java.awt.Font;
+import java.sql.Time;
 import java.util.ArrayList;
+import java.util.Random;
 
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
@@ -9,26 +13,30 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
 import org.newdawn.slick.geom.Vector2f;
 
 public class TestScreen extends BasicGame{
 	Player player;
 	ArrayList<Bullet> bulletList;
 	ArrayList<Enemy> enemyList;
-	
+
 	
 	//*************************ST**************************
-	Icon key;
+	//Shape rectangle;
 	IconXAmount keyXAmount;
+	IconXAmount FreshmenChestXAmount;
+	IconXAmount SophomoreChestXAmount;
+	IconXAmount JuniorChestXAmount;
+	IconXAmount SeniorChestXAmount;
 	
 	/*public static Color color1 = Color.white;
 	public static Color color2 = Color.blue;
 	public static Color color3 = Color.red;*/
-	
-	//ArrayList<Layer> layerList;
-	
-	
+
 	ArrayList<MultiBar> multiBarList;
+	ArrayList<Key> keyList;
 	//*************************ST**************************
 	
 	
@@ -44,12 +52,13 @@ public class TestScreen extends BasicGame{
 	public void handleCollisions()
 	{
 		handleEnemyBulletCollisions();
-		if(player!=null)
+		if(player!=null){
 			handleEnemyPlayerCollisions();
+			//*************************ST**************************
+			handleKeyCollisions();
+			//*************************ST**************************
+		}
 		handleEnemyEnemyCollisions();
-		//*************************ST**************************
-		handleKeyCollisions();
-		//*************************ST**************************
 	}
 	
 	//To avoid the enemies to go through each other.
@@ -85,7 +94,6 @@ public class TestScreen extends BasicGame{
 				player.bounceOff(enemyList.get(i), 30f);
 			}
 		}
-		
 		handleRemovals((ArrayList)enemyList);
 	}
 
@@ -106,31 +114,43 @@ public class TestScreen extends BasicGame{
 		handleRemovals((ArrayList)enemyList);
 		
 		
+		
 		//*************************ST**************************
 		//handleRemovals((ArrayList)layerList);
 		handleRemovals((ArrayList)multiBarList);
 		//*************************ST**************************
-		
-		
 	}
+	
+	
+	
 	//*************************ST**************************
-	private void handleKeyCollisions(){
-		/*if(player.collidesKey(key))
-		{
-			player.takeKey();
-			key.setToBeRemoved(true);
-		}	*/	
+	private void handleKeyCollisions()
+	{
+		for(int i=0; i<keyList.size(); i++){
+			if(player.collides(keyList.get(i)))
+			{
+				player.takeKey();
+				keyList.get(i).setToBeRemoved(true);
+				keyXAmount.amount++;	
+			}
+			
+		}
+		//System.out.println(myKey.isToBeRemoved());
+		handleRemovals((ArrayList)keyList);
 	}
 	//*************************ST**************************
 	
+	
+	
 	private void handleRemovals(ArrayList<GameObject> list) {
 		int size = list.size();
-		for (int i = 0; i< list.size(); i++)
+		for (int i = 0; i< size; i++)
 		{
 			if(list.get(i).isToBeRemoved())
 			{
 				list.remove(i);
 				size--;
+				System.out.println(list);
 			}
 		}
 	}
@@ -139,60 +159,83 @@ public class TestScreen extends BasicGame{
 		
 		Input input = container.getInput();
 		
-	      //up
-	      if(input.isKeyDown(Input.KEY_W)){
-	         player.setUp(true);
-	      }
-	      else
-	    	  player.setUp(false);
-	      
-	      //down
-	      if(input.isKeyDown(Input.KEY_S)){
-	    	  player.setDown(true);
-	      }
-	      else
-	    	  player.setDown(false);
-	      
-	      //left
-	      if(input.isKeyDown(Input.KEY_A)){
-	    	  player.setLeft(true);
-	      }
-	      else
-	    	  player.setLeft(false);
-	      
-	      //right
-	      if(input.isKeyDown(Input.KEY_D)){
-	    	  player.setRight(true);
-	      }
-    	  else
-    		  player.setRight(false);
-	      
-	      //mouse
-	      if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON))
-		  {
-	    	  Bullet bullet = player.shoot(new Vector2f(input.getMouseX(), input.getMouseY()));
-	    	  if (bullet != null)
-	    		  bulletList.add(bullet);
-		  }	
-	      if (input.isMousePressed(Input.MOUSE_RIGHT_BUTTON))
-		  {
-	    	  player.setPowerUpActive(!player.isPowerUpActive());
-	    		  
-		  }	  
+		//up
+		if(input.isKeyDown(Input.KEY_W)){
+			player.setUp(true);
+		}
+		else{
+			player.setUp(false);
+		}
+	  
+		//down
+		if(input.isKeyDown(Input.KEY_S)){
+			player.setDown(true);
+		}
+		else{
+			player.setDown(false);
+		}
+	  
+		//left
+		if(input.isKeyDown(Input.KEY_A)){
+			player.setLeft(true);
+		}
+		else{
+			player.setLeft(false);
+		}
+	  
+		//right
+		if(input.isKeyDown(Input.KEY_D)){
+			player.setRight(true);
+		}
+		else{
+			player.setRight(false);
+		}
+	  
+		//mouse
+		if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON))
+		{
+			Bullet bullet = player.shoot(new Vector2f(input.getMouseX(), input.getMouseY()));
+			if (bullet != null){
+				bulletList.add(bullet);
+			}
+		}	
+	  
+		if (input.isMousePressed(Input.MOUSE_RIGHT_BUTTON))
+		{
+			player.setPowerUpActive(!player.isPowerUpActive());	  
+		}	  
 	}
 
+	
+	
+	//*************************ST**************************
+	private void spawnKey(){
+		Random rnd = new Random();
+		float velX = rnd.nextFloat()*800f;
+		float velY = rnd.nextFloat()*600f;
+		Vector2f v = new Vector2f(velX, velY);
+		Key k;
+		try {
+			k = new Key(v, 500, 500);
+			keyList.add(k);
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+	}
+	//*************************ST**************************
+	
+	
+	
 	@Override
-	public void init(GameContainer container) throws SlickException {
+	public void init(GameContainer container) throws SlickException 
+	{
 		player = new Player (new Vector2f(400f, 400f), new Vector2f(30f, 30f), 
 				3f, 100f, 6f, 5f, 
 				200f, 0f, container.getWidth(), 
 				container.getHeight());
 		bulletList = new ArrayList<Bullet>();
 		enemyList = new ArrayList<Enemy>();
-		
-		
-		
-		
+
 		/*
 		Enemy enemy1 = new Enemy(new Vector2f(300f, 300f), new Vector2f(30f, 30f), 
 				2f, 100f, 100f, 3f, player);
@@ -204,16 +247,17 @@ public class TestScreen extends BasicGame{
 		*/
 		
 		
+		
 		//*************************ST**************************
 		//layerList = new ArrayList<Layer>();
 		multiBarList = new ArrayList<MultiBar>();
+		keyList = new ArrayList<Key>();
 		
 		multiBarList.add(new MultiBar(player, false)); //not horizontal
 		
-		
-		
+		//enemy1
 		Enemy enemy1 = new Enemy(new Vector2f(300f, 300f), new Vector2f(30f, 30f), 
-								2.5f, 100f, 100f, 3f, player);
+								2.5f, 100f, 3f, player);
 		enemyList.add(enemy1);
 		//MultiBar mb1 = new MultiBar(enemy1, true);
 		multiBarList.add(new MultiBar(enemy1, true));
@@ -223,8 +267,9 @@ public class TestScreen extends BasicGame{
 /*      layerList.add(new Layer(color1, enemy1.enemyStats.getMaxHealth(), enemy1, true));
 		layerList.add(new Layer(color2, enemy1.enemyStats.getCurHealth(), enemy1, false));*/
 		
+		//enemy2
 		Enemy enemy2 = new Enemy(new Vector2f(900f, 300f), new Vector2f(30f, 30f), 
-								2.5f, 100f, 100f, 3f, player);
+								2.5f, 100f, 3f, player);
 		enemyList.add(enemy2);
 		//MultiBar mb2 = new MultiBar(enemy2, true);
 		multiBarList.add(new MultiBar(enemy2, true));
@@ -234,8 +279,9 @@ public class TestScreen extends BasicGame{
 /*		layerList.add(new Layer(color1, enemy2.enemyStats.getMaxHealth(), enemy2, true));
 		layerList.add(new Layer(color2, enemy2.enemyStats.getCurHealth(), enemy2, false));*/
 		
+		//enemy3
 		Enemy enemy3 =new Enemy(new Vector2f(100f, 100f), new Vector2f(30f, 30f),
-								0.5f, 100f, 100f, 1f, player);
+								0.5f, 100f, 1f, player);
 		enemyList.add(enemy3);
 		//MultiBar mb3 = new MultiBar(enemy3, true);
 		multiBarList.add(new MultiBar(enemy3, true));
@@ -250,23 +296,56 @@ public class TestScreen extends BasicGame{
 								new Icon(new Image("res/keyX.png")), 
 								0 ); //it will change with the amount of keys player has
 		
-		key = new Icon(new Image("res/key.png"));
+		FreshmenChestXAmount = new IconXAmount(new Vector2f(100f,0f), 
+								new Vector2f(0f,0f), 
+								new Icon(new Image("res/FreshmenchestX.png")), 
+								0 ); //it will change with the amount of chest player has
+		
+		SophomoreChestXAmount = new IconXAmount(new Vector2f(200f,0f), 
+								new Vector2f(0f,0f), 
+								new Icon(new Image("res/SophomoreChestX.png")), 
+								0 ); //it will change with the amount of chest player has
+
+		
+		JuniorChestXAmount = new IconXAmount(new Vector2f(300f,0f), 
+								new Vector2f(0f,0f), 
+								new Icon(new Image("res/JuniorChestX.png")), 
+								0 ); //it will change with the amount of chest player has
+
+		
+		SeniorChestXAmount = new IconXAmount(new Vector2f(400f,0f), 
+								new Vector2f(0f,0f), 
+								new Icon(new Image("res/SeniorChestX.png")), 
+								0 ); //it will change with the amount of chest player has
+		
+		Random rnd = new Random();
+		float velX = rnd.nextFloat()*800f;
+		float velY = rnd.nextFloat()*600f;
+		keyList.add(new Key(new Vector2f(velX, velY), 500, 500));
+		
+		//if(keyList.size()<1)
+			//dkeyList.add(new Key(new Vector2f(60f,260f),400,400));
+		/*keyList.add(new Key(new Vector2f(160f,160f)));
+		keyList.add(new Key(new Vector2f(360f,260f)));
+		keyList.add(new Key(new Vector2f(460f,60f)));*/
 		
 		//*************************ST**************************
-		
-		
+
 	}
+	
+	
 	
 	@Override
 	public void update(GameContainer container, int arg2) throws SlickException 
 	{
-		if(player !=null)
-		{
+		if(player !=null){
 			manageInput(container);
 			player.update();
 		}
-		for (int i =0; i < bulletList.size(); i++)
+		
+		for (int i =0; i < bulletList.size(); i++){
 			bulletList.get(i).update();
+		}
 		
 		for (int i =0; i < enemyList.size(); i++){
 			enemyList.get(i).update();
@@ -277,29 +356,38 @@ public class TestScreen extends BasicGame{
 			System.out.println("Wow");*/
 		//System.out.println(player.getPosition().getX() + ", " + player.getPosition().getY());
 		//enemy.findTarget(player);
+		
 		handleCollisions();
-			
+		
+		
 		
 		//*************************ST**************************
-		// it will change with the if(player collides any key on the game screen)
 		
-		//if(player.collidesKey(key)){
-			keyXAmount.update();
-		//}
+		keyXAmount.update();
+		FreshmenChestXAmount.update();
+		SophomoreChestXAmount.update();
+		JuniorChestXAmount.update();
+		SeniorChestXAmount.update();
 		
-		/*for (int i =0; i < layerList.size(); i++)
-			layerList.get(i).update();*/
-		
-		
-		for (int i =0; i < multiBarList.size(); i++){
+		for (int i=0; i<multiBarList.size(); i++){
 			multiBarList.get(i).update();
 		}
 		
+		for (int i=0; i<keyList.size(); i++){
+			keyList.get(i).update();
+		}
+		
+		if(keyList.size() < 1){
+			spawnKey();
+		}
 		
 		//*************************ST**************************
 		
-		if (player != null && player.getStats().isDead())
+		
+		
+		if (player != null && player.getStats().isDead()){
 			player= null;
+		}
 
 	}
 
@@ -307,32 +395,52 @@ public class TestScreen extends BasicGame{
 	public void render(GameContainer container, Graphics g) throws SlickException 
 	{
 		g.setBackground(BACKGROUND);
-		if(player !=null)
-			player.draw(g);
-		if(player == null)
-			g.drawString("GAME OVER!", container.getWidth()/2, container.getHeight()/2);
-		for (int i =0; i < bulletList.size(); i++)
-			bulletList.get(i).draw(g);
-		for (int i =0; i < enemyList.size(); i++)
-			enemyList.get(i).draw(g);
+		
 		
 		
 		//*************************ST**************************
 		keyXAmount.draw(g);
+		FreshmenChestXAmount.draw(g);
+		SophomoreChestXAmount.draw(g);
+		JuniorChestXAmount.draw(g);
+		SeniorChestXAmount.draw(g);
 		
 		/*for (int i =0; i < layerList.size(); i++)
 			layerList.get(i).draw(g);*/
 		
-		
-		for (int i =0; i < multiBarList.size(); i++)
+		for (int i =0; i < multiBarList.size(); i++){
 			multiBarList.get(i).draw(g);
+		}
+
+		for (int i =0; i < keyList.size(); i++){
+			keyList.get(i).draw(g);
+		}
+
+		g.setColor(Color.white);
 		
-		key.draw(g);
+		String scoreLabel = "SCORE : " + player.score;	
+		//g.setFont( (org.newdawn.slick.Font) new Font( "TimesRoman" , Font.PLAIN, 18 ));
+		g.drawString(scoreLabel, 980f, 5f);
 		
 		//*************************ST**************************
-
 		
+		
+		
+		if(player !=null){
+			player.draw(g);
+		}
+		if(player == null){
+			g.drawString("GAME OVER!", container.getWidth()/2, container.getHeight()/2);
+		}
+		for (int i =0; i < bulletList.size(); i++){
+			bulletList.get(i).draw(g);
+		}
+		for (int i =0; i < enemyList.size(); i++){
+			enemyList.get(i).draw(g);
+		}
+	
 	}
+
 	
 	public static void main(String[] args) throws SlickException {
 		AppGameContainer app = new AppGameContainer(new TestScreen ("Test"));
@@ -341,4 +449,5 @@ public class TestScreen extends BasicGame{
 		app.setTargetFrameRate(FPS);
 		app.start();
 	}
+	
 }
