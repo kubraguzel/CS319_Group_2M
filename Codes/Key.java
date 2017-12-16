57a8325bd3b04f6618ee494a6c8f9ae951f91d24
semihteker <32205459+semihteker@users.noreply.github.com>
2017-12-16 23:14:23 +0300

    Update Key.java

--- Codes/Key.java
+++ Codes/Key.java
@@ -10,7 +10,6 @@ import java.util.Random;
 
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
-import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Vector2f;
 
@@ -20,21 +19,21 @@ public class Key extends Bonus {
 	//Vector2f dimension;
 	float duration;
 	float remainingTime;
-	Image img = new Image("res/key.png");
+	Image img;
 	Icon keyIcon;
 	
-	public Key(Vector2f position) throws SlickException {
+	public Key(Vector2f position, Image img) {
 		this.position=position;
 		//this.dimension=dimension;
-		//img = new Image("res/key.png");
+		this.img = img;
 		keyIcon = new Icon(position, img);
 		super.shape = new Rectangle(position.x, position.y, img.getWidth(), img.getHeight());
 	}
 	
-	public Key(Vector2f position, float duration, float remainingTime) throws SlickException {
+	public Key(Vector2f position, Image img, float duration, float remainingTime){
 		this.position=position;
 		//this.dimension=dimension;
-		//img = new Image("res/key.png");
+		this.img = img;
 		this.duration = duration;
 		this.remainingTime = remainingTime;
 		keyIcon = new Icon(position, img);
@@ -52,7 +51,7 @@ public class Key extends Bonus {
 	@Override
 	public void draw(Graphics g) {
 		if(!pickedUp){
-			keyIcon.draw(g);
+			img.draw(position.x, position.y);
 		}	
 	}
 	
@@ -73,4 +72,12 @@ public class Key extends Bonus {
 		}
 	}
 
+	public Image getImg() {
+		return img;
+	}
+
+	public void setImg(Image img) {
+		this.img = img;
+	}
+	
 }
