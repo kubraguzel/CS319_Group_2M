2141ed0603d2e89c4c0ab62f869469484126d91d
semihteker <32205459+semihteker@users.noreply.github.com>
2017-12-16 23:15:28 +0300

    Update Layer.java

--- Codes/Layer.java
+++ Codes/Layer.java
@@ -5,69 +5,131 @@ import org.newdawn.slick.geom.Vector2f;
 public class Layer extends UIObject{
 	Vector2f position;
 	Vector2f dimension;
-	public Color color1;
-	public Color color2;
+	public Color color;
+	//public Color color2;
+	boolean boxLayer;
 	boolean horizontal = true;
 	float percentage;
+	float rate;
 	Enemy enemy;
+	Player player;
+	boolean isPlayer = false;
 	
-	public Layer(Vector2f position, Vector2f dimension, Color color1, Color color2,  float percentage) {
+	public Layer(Vector2f position, Vector2f dimension, Color color,  float percentage) {
 		super(position, dimension);
-		this.color1 = color1;
-		this.color2 = color2;	
+		this.color = color;
+		//this.color2 = color2;	
 		this.percentage = percentage;
-		// TODO Auto-generated constructor stub
 	}
 	
-	public Layer(Color color1, Color color2,  float percentage, Enemy enemy) {
+	public Layer(Color color, float percentage, Enemy enemy, boolean boxLayer) {
 		position = enemy.getPosition().add(new Vector2f(-15f, -15f));
-		dimension = new Vector2f(1f,1f); 
-		this.color1 = color1;
-		this.color2 = color2;
+		dimension = new Vector2f(1f,2.5f); 
+		this.color = color;
+		//this.color2 = color2;
 		this.percentage = percentage;
 		this.enemy = enemy;
-		// TODO Auto-generated constructor stub
+		this.boxLayer = boxLayer;
+	}
+	
+	public Layer(Color color, float percentage, Player player, boolean boxLayer) {
+		position =  new Vector2f(1880f, 940f);
+		dimension = new Vector2f(8.5f,-8.5f);
+		this.color = color;
+		//this.color2 = color2;
+		this.percentage = percentage;
+		this.player = player;
+		this.boxLayer = boxLayer;
+		isPlayer = true;
+		horizontal = false;
 	}
 	
 	@Override
 	public void draw(Graphics g) 
 	{
-		g.setColor(color1);
-		g.fillRect(position.getX(), position.getY(), dimension.getX(), dimension.getY());
-		g.setColor(color2);
-		
 		if(horizontal){
-			g.fillRect(position.getX(),position.getY(), (float) dimension.getX()*percentage, dimension.getY());
+			if(boxLayer){
+				g.setColor(color);
+				g.fillRect(position.getX(), position.getY(), dimension.getX()*100f, dimension.getY());
+			}
+			else{
+				g.setColor(color);
+				g.fillRect(position.getX(),position.getY(), (float) dimension.getX()*percentage, dimension.getY());
+			}
 		}
 		else{
-			g.fillRect(position.getX(),position.getY(), dimension.getX(), (float) dimension.getY()*percentage);
+			if(boxLayer){
+				g.setColor(color);
+				g.fillRect(position.getX(),position.getY(), dimension.getX(), (float) dimension.getY()*100f);
+			}
+			else{
+				g.setColor(color);
+				g.fillRect(position.getX(),position.getY(), dimension.getX(), (float) dimension.getY()*percentage);
+			}
 		}
 	}
 	
 	public void move() {
 		if (enemy != null){
-			setPosition(new Vector2f(enemy.getPosition().getX()-50f, enemy.getPosition().getY()-20f));
+			if(enemy.numOfLayer>2){
+				setPosition(new Vector2f(enemy.getPosition().getX()-50f, enemy.getPosition().getY()-100f));	
+			}
+			else{
+				setPosition(new Vector2f(enemy.getPosition().getX()-50f, enemy.getPosition().getY()-20f));
+			}
 		}
-		else
+		else{
 			this.setToBeRemoved(true);
+		}
 	}
 
 	public void setPosition(Vector2f pos) {
-		this.position = pos;
-		// TODO Auto-generated method stub
-		
+		this.position = pos;		
 	}
 	
 	public void setPercentage(){
-		this.percentage = enemy.enemyStats.getCurHealth();
+		
+		if(isPlayer){
+			rate = (player.playerStats.getCurHealth()/player.playerStats.getMaxHealth())*100;
+			this.percentage = rate;
+		}
+		else{
+			if(enemy.numOfLayer <= 2){
+				rate = (enemy.enemyStats.getCurHealth()/enemy.enemyStats.getMaxHealth())*100;
+				this.percentage = rate;
+			}
+			else{
+				rate = (enemy.enemyStats.getCurHealth()/enemy.enemyStats.getMaxHealth())*100;
+				if(rate>50){
+					this.percentage = ((rate-50)/50)*100;
+				}
+				else{
+					this.percentage = (rate/50)*100;
+				}
+			}
+		}
+
+		if(this.percentage == 0){
+			setToBeRemoved(true);
+		}
 	}
 
+	@Override
 	public void update() 
 	{
 		setPercentage();
-		if(enemy.isToBeRemoved())
-			setToBeRemoved(true);
-		else
-			move();
+		if(isPlayer){
+			if(player.isToBeRemoved()){
+				setToBeRemoved(true);
+			}
+		}
+		else{
+			if(enemy.isToBeRemoved()){
+				setToBeRemoved(true);
+			}
+			else{
+				move();
+			}
+		}
 	}
 }
