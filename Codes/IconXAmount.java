70a4e94466df8d9b6f2db547e1c0228cb3cb8587
semihteker <32205459+semihteker@users.noreply.github.com>
2017-12-16 03:35:11 +0300

    Add files via upload

--- Codes/IconXAmount.java
+++ Codes/IconXAmount.java
@@ -1,3 +1,10 @@
+package deneme;
+
+/**
+ * 
+ * Author: Semih Teker
+ * 
+ */
 
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
@@ -6,9 +13,13 @@ import org.newdawn.slick.geom.Vector2f;
 public class IconXAmount extends UIObject{
 	Icon icon;
 	int amount = 0; 
-		
+	Vector2f position;
+	Vector2f dimension;
+	
 	public IconXAmount(Vector2f position, Vector2f dimension, Icon icon, int amount ) {
-		super(position, dimension);
+		this.position = position;
+		this.dimension = dimension;
+		//super(position, dimension);
 		this.icon = icon;
 		this.amount = amount;
 		// TODO Auto-generated constructor stub
@@ -16,13 +27,14 @@ public class IconXAmount extends UIObject{
 
 	@Override
 	public void update(){
-		amount++;
+		
 	}
 	
 	@Override
 	public void draw(Graphics g){
-		g.drawImage(icon.getImage(), 0, 0, Color.gray); // 25*80 pixels at x=0 and y=0
-		g.drawString(amount + "", 60, 3); // draw at x=60 and y=3
+		g.setColor(Color.black);
+		g.drawImage(icon.getImage(), position.x, position.y, Color.gray); // 25*90 pixels at x=0 and y=0
+		g.drawString(amount + "", position.x+60, 3); // draw at x=60 and y=3
 	}
 	
 	public Icon getIcon(){
